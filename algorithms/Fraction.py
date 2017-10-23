ac80f9b4f2832c4c9dd941c27a920de11a169594
Vijayarajan Govindarajan <vgovindarajan@drw.com>
2017-10-23 17:44:34 -0500

    Adding methods to Fraction class

--- algorithms/Fraction.py
+++ algorithms/Fraction.py
@@ -1,6 +1,16 @@
 # http://interactivepython.org/runestone/static/pythonds/Introduction/ObjectOrientedProgramminginPythonDefiningClasses.html
 # Vijayarajan Govindarajan 2017
 
+
+def gcd(num, den):
+    while num % den != 0:
+        oldnum = num
+        oldden = den
+
+        num = oldden
+        den = oldnum % oldden
+    return den
+
 class Fraction:
     def __init__(self, num, den):
         if den == 0:
@@ -8,16 +18,38 @@ class Fraction:
         self.num = num
         self.den = den
 
-    def show(self):
-        print(self.num, "/", self.den)
+    def __str__(self):
+        return "%d / %d" % (self.num, self.den)
 
+    def __add__(self, other):
+        newnum = (self.num * other.den) + (self.den * other.num)
+        newden = self.den * other.den
+        common = gcd(newnum, newden)
+        return Fraction(newnum / common, newden / common)
+
+    def __sub__(self, other):
+        newnum = (self.num * other.den) - (self.den * other.num)
+        newden = self.den * other.den
+        common = gcd(newnum, newden)
+        return Fraction(newnum / common, newden / common)
+
+    def __eq__(self, other):
+        left = self.num * other.den
+        right = self.den * other.num
+        return left == right
 
 def main():
-    fraction = Fraction(3, 7)
-    print(fraction.num)
-    print(fraction.den)
-    print(fraction)
-
+    twofifths = Fraction(2, 5)
+    print("twofifths", twofifths)
+    onefifths = Fraction(1, 5)
+    print("onefifths", onefifths)
+    threefifths = twofifths + onefifths
+    print(twofifths, "+", onefifths, " = ", threefifths)
+    subonefifths = twofifths - onefifths
+    print(twofifths, "-", onefifths, " = ", subonefifths)
+    sixtenths = Fraction(6, 10)
+    print("sixtenths", sixtenths)
+    print(threefifths, " & ", sixtenths, "are equal ? ", threefifths == sixtenths)
 
 if __name__ == "__main__":
     main()
