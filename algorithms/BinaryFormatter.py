ed2931b82f9c943523faab20853ca07dff2deeaa
Vijayarajan Govindarajan <vgovindarajan@drw.com>
2017-11-08 17:38:30 -0600

    Added infix to postfix converter for simple expressions using Stack impl

--- algorithms/BinaryFormatter.py
+++ algorithms/BinaryFormatter.py
@@ -3,6 +3,45 @@
 
 from StackImpl import StackImpl
 
+postFixPrec = {}
+postFixPrec["*"] = 3
+postFixPrec["/"] = 3
+postFixPrec["+"] = 2
+postFixPrec["-"] = 2
+postFixPrec["("] = 1
+
+alphabets = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+numbers = ("0","1","2","3","4","5","6","7","8","9")
+
+def infixToPostfix(infixExpression):
+    operatorStack = StackImpl()
+    postFixList = []
+
+    tokenList = infixExpression.split()
+    for token in tokenList:
+        if token in alphabets or token in numbers:
+            postFixList.append(token)
+        elif token == '(':
+            #opening bracket
+            operatorStack.push(token)
+        elif token == ')':
+            #closing bracket
+            topToken = operatorStack.pop()
+            while topToken != '(':
+                postFixList.append(topToken)
+                topToken = operatorStack.pop()
+        else:
+            while (not operatorStack.isEmpty() and \
+                    postFixPrec[operatorStack.peek()] >= postFixPrec[token]):
+                    postFixList.append(operatorStack.pop())
+            operatorStack.push(token)
+
+    while not operatorStack.isEmpty():
+        postFixList.append(operatorStack.pop())
+
+    return " ".join(postFixList)
+
+
 def baseConverter(integer, base):
     reminderStack = StackImpl()
     digits = "0123456789ABCDEF"
@@ -33,15 +72,19 @@ def toBinary(base10Integer):
     return binaryString
 
 def main():
-    base10Integer = 42
+    base10Integer = 256
     binaryValue = toBinary(base10Integer)
-    binaryValueNew = baseConverter(42,2)
-    octaValue = baseConverter(42,8)
-    hexaValue = baseConverter(42,16)
+    binaryValueNew = baseConverter(base10Integer,2)
+    octaValue = baseConverter(base10Integer,8)
+    hexaValue = baseConverter(base10Integer,16)
     print("Base 10 ", base10Integer, " Binary ", binaryValue)
     print("Base 10 ", base10Integer, " Binary New ", binaryValueNew)
     print("Base 10 ", base10Integer, " Octal ", octaValue)
     print("Base 10 ", base10Integer, " HexaValue ", hexaValue)
+
+    infix = "( A * B ) * ( C * D ) + D * E "
+    postFix = infixToPostfix(infix)
+    print(infix," in postfix is = ", postFix)
 
 if __name__ == "__main__":
     main()
