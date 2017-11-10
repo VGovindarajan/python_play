84c1521261f8d1dc5c8a5ebd1f0d19c846f28bbb
Vijayarajan Govindarajan <vgovindarajan@drw.com>
2017-11-10 09:54:56 -0600

    Added post fix eval function

--- algorithms/BinaryFormatter.py
+++ algorithms/BinaryFormatter.py
@@ -1,4 +1,5 @@
 #https://runestone.academy/runestone/static/pythonds/BasicDS/ConvertingDecimalNumberstoBinaryNumbers.html
+#https://runestone.academy/runestone/static/pythonds/BasicDS/InfixPrefixandPostfixExpressions.html?lastPosition=2624
 # Vijayarajan Govindarajan 2017
 
 from StackImpl import StackImpl
@@ -12,6 +13,7 @@ postFixPrec["("] = 1
 
 alphabets = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
 numbers = ("0","1","2","3","4","5","6","7","8","9")
+operators = ("*","/","+","-")
 
 def infixToPostfix(infixExpression):
     operatorStack = StackImpl()
@@ -71,6 +73,33 @@ def toBinary(base10Integer):
 
     return binaryString
 
+
+def doTheMath(operator, operand1, operand2):
+    if operator == "*":
+        return operand1*operand2
+    elif operator == "/":
+        return operand1/operand2
+    elif operator == "+":
+        return operand1+operand2
+    else:
+        return operand1-operand2
+
+
+def postFixEvaluation(postFixExpression):
+    operandStack = StackImpl()
+    tokenList = postFixExpression.split()
+
+    for token in tokenList:
+        if token in numbers:
+            operandStack.push(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doTheMath(token, operand1, operand2)
+            operandStack.push(result)
+
+    return operandStack.pop()
+
 def main():
     base10Integer = 256
     binaryValue = toBinary(base10Integer)
@@ -83,8 +112,23 @@ def main():
     print("Base 10 ", base10Integer, " HexaValue ", hexaValue)
 
     infix = "( A * B ) * ( C * D ) + D * E "
+    infix2 = "( A + B ) * C - ( D - E ) * ( F + G )"
     postFix = infixToPostfix(infix)
     print(infix," in postfix is = ", postFix)
+    print(infix2," in postfix is = ", infixToPostfix(infix2))
+
+    infix3 = "( 7 + 8 ) / ( 3 + 2 )"
+    postfix3 = infixToPostfix(infix3)
+    postfixEval3 = postFixEvaluation(postfix3)
+    print(infix3," in postfix is = ", postfix3 , " eval is  ", postfixEval3 )
+
+    infix3 = "( 7 * 8 ) - ( 2 * 2 ) * 6 + 3 * 2"
+    postfix3 = infixToPostfix(infix3)
+    postfixEval3 = postFixEvaluation(postfix3)
+    print(infix3," in postfix is = ", postfix3 , " eval is  ", postfixEval3 )
+
+
+
 
 if __name__ == "__main__":
     main()
