3153fb30ea0f3536510dc0d48a00b594c7e4ccb1
Vijayarajan Govindarajan <vgovindarajan@drw.com>
2017-11-12 18:34:59 -0600

    Added palindrome checker

--- algorithms/DequeImpl.py
+++ algorithms/DequeImpl.py
@@ -29,6 +29,23 @@ class DequeImpl:
     def removeRear(self):
         return self.items.pop(0)
 
+
+def palindromeChecker(strToCheck):
+    deq = DequeImpl()
+
+    for ch in strToCheck:
+        deq.addBack(ch)
+
+    stillEqual = True
+
+    while deq.size() > 1 and stillEqual:
+        front = deq.removeFront()
+        rear = deq.removeRear()
+        if front != rear:
+            stillEqual = False
+    return stillEqual
+
+
 def main():
     d = DequeImpl()
     d.addFront(1)
@@ -37,6 +54,19 @@ def main():
     print(d)
     while not d.isEmpty():
         print(str(d.removeFront()))
+
+    pa1 = "MadaM"
+    pa2 = "AmmA"
+    pa3 = "Malayalam"
+    pa4 = "malayalam"
+    pa5 = "radar"
+
+    print(pa1, " is a palindrome ? ", palindromeChecker(pa1))
+    print(pa2, " is a palindrome ? ", palindromeChecker(pa2))
+    print(pa3, " is a palindrome ? ", palindromeChecker(pa3))
+    print(pa4, " is a palindrome ? ", palindromeChecker(pa4))
+    print(pa5, " is a palindrome ? ", palindromeChecker(pa5))
+
 
 if __name__ == "__main__":
     main()
