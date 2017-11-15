46f8167b5055b6259b7f5f57f8df6755d9e6b038
Vijayarajan Govindarajan <vgovindarajan@drw.com>
2017-11-15 17:57:44 -0600

    Added functions for Ordered Link List Impl

--- algorithms/OrderedLinkListImpl.py
+++ algorithms/OrderedLinkListImpl.py
@@ -1,37 +1,51 @@
-# https://runestone.academy/runestone/static/pythonds/BasicDS/ImplementinganUnorderedListLinkedLists.html
-# Vijayarajan Govindarajan 2017
+#https://runestone.academy/runestone/static/pythonds/BasicDS/TheOrderedListAbstractDataType.html
+#Vijayarajan Govindarajan 2017
+#Based on LinkedListImpl.py
 
 '''
-List() creates a new list that is empty. It needs no parameters and returns an empty list.
-add(item) adds a new item to the list. It needs the item and returns nothing. Assume the item is not already in the list.
+OrderedList() creates a new ordered list that is empty. It needs no parameters and returns an empty list.
+add(item) adds a new item to the list making sure that the order is preserved. It needs the item and returns nothing. Assume the item is not already in the list.
 remove(item) removes the item from the list. It needs the item and modifies the list. Assume the item is present in the list.
 search(item) searches for the item in the list. It needs the item and returns a boolean value.
 isEmpty() tests to see whether the list is empty. It needs no parameters and returns a boolean value.
 size() returns the number of items in the list. It needs no parameters and returns an integer.
-append(item) adds a new item to the end of the list making it the last item in the collection. It needs the item and returns nothing. Assume the item is not already in the list.
 index(item) returns the position of item in the list. It needs the item and returns the index. Assume the item is in the list.
-insert(pos,item) adds a new item to the list at position pos. It needs the item and returns nothing. Assume the item is not already in the list and there are enough existing items to have position pos.
 pop() removes and returns the last item in the list. It needs nothing and returns an item. Assume the list has at least one item.
 pop(pos) removes and returns the item at position pos. It needs the position and returns the item. Assume the item is in the list.
 '''
 
 from Node import Node
 
-class UnorderedListImpl:
+class OrderedLinkListImpl:
     def __init__(self):
         self.head = None
 
     def __repr__(self):
         return str(self.head)
 
-
     def isEmpty(self):
         return True if self.head is None else False
 
     def add(self, data):
+        previous = None
+        current = self.head
+        stop = False
+
+        while current is not None and stop is False:
+            if current.getData() > data :
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+
         n = Node(data)
-        n.setNext(self.head)
-        self.head = n
+
+        if previous is None:
+            n.setNext(self.head)
+            self.head = n
+        else:
+            n.setNext(current)
+            previous.setNext(n)
 
     def size(self):
         current = self.head
@@ -44,11 +58,15 @@ class UnorderedListImpl:
     def search(self, item):
         current = self.head
         found = False
-        while current is not None and found is not True:
+        stop = False
+        while current is not None and found is not True and stop is not True:
             if current.getData() == item:
                 found = True
             else:
-                current = current.getNext()
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
         return found
 
     def remove(self, item):
@@ -71,21 +89,6 @@ class UnorderedListImpl:
 
         return found
 
-    def append(self, item):
-        current = self.head
-        previous = None
-
-        while current is not None:
-            previous = current
-            current = current.getNext()
-
-        n = Node(item)
-
-        if previous is None:
-            self.head = n
-        else:
-            previous.setNext(n)
-
     def index(self, item):
         current = self.head
         found = False
@@ -102,29 +105,6 @@ class UnorderedListImpl:
             return position
         else:
             return invalidPosition
-
-    def insert(self, pos, item):
-        invalidPosition = -1
-        size = self.size()
-        if size < pos:
-            #print("LinkedList size is ", size, " cannot insert at ",  pos)
-            return invalidPosition
-        current = self.head
-        previous = None
-        position = 0
-        while current is not None and pos != position:
-            previous = current
-            current = current.getNext()
-            position += 1
-
-        node = Node(item)
-        node.setNext(current)
-        if previous is None:
-            self.head = node
-        else:
-            previous.setNext(node)
-
-        return position
 
     def pop(self, *args):
         invalidPosition = -1
@@ -154,47 +134,33 @@ class UnorderedListImpl:
         return current.getData()
 
 def main():
-    u = UnorderedListImpl()
-    u.add(111)
-    u.add(222)
-    u.add(333)
+    ol = OrderedLinkListImpl()
+    ol.add(120)
+    ol.add(220)
+    ol.add(12)
+    ol.add(1)
 
-    print(u)
-    print(u.isEmpty)
-    print(u.size())
+    print(ol)
 
-    print("is 122 available ?", u.search(122))
-    print("index of 122 ?", u.index(122))
-    print("Removed first 122 if available ?", u.remove(122))
+    print("search(220)", ol.search(220))
+    print(ol)
+    print("search(234)", ol.search(234))
+    print(ol)
 
-    print("is 222 available ?", u.search(222))
-    print("index of 222 ?", u.index(222))
-    # print(u[1])
-    print("Removed first 222 if available ?", u.remove(222))
+    print("pop()", ol.pop())
+    print(ol)
+    print("pop(2)", ol.pop(2))
+    print(ol)
 
-    print(u)
+    print("index(1)", ol.index(1))
+    print(ol)
 
-    u.append(444)
+    print("remove(220)", ol.remove(220))
+    print(ol)
+    print("remove(12)", ol.remove(12))
+    print(ol)
 
-    print(u)
 
-    ul = UnorderedListImpl()
-    print("pop() on new list", ul.pop())
-    ul.append(555)
-    ul.insert(3,111)
-    ul.insert(0, 222)
-    ul.insert(1, 333)
-    ul.insert(3, 444)
-    ul.insert(0, "baa")
-    print(ul)
-
-    print("pop at index 7", ul.pop(7))
-    print("pop at index 0",ul.pop(0))
-    print(ul)
-    print("pop at index 3",ul.pop(3))
-    print(ul)
-    print("pop() on list", ul.pop())
-    print(ul)
 
 if __name__ == "__main__":
     main()
